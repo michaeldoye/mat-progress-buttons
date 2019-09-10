3760dcfa977220f2cb585e9552b1b929b4df3047
Augustin Husson <augustin.husson@amadeus.com>
2019-09-10 12:32:17 +0200

    upgrading dependency on demo side + removing usage of HTTPModule which is no longer supported by Angular

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { TransferHttpCacheModule } from '@nguniversal/common';
@@ -10,22 +9,22 @@ import { HomeModule } from './home/home.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        // Add .withServerTransition() to support Universal rendering.
-        // The application ID can be any identifier which is unique on
-        // the page.
-        BrowserModule.withServerTransition({ appId: 'mat-progress-buttons-demo-id' }),
-        TransferHttpCacheModule,
-        FormsModule,
-        HttpModule,
-        AppRoutingModule,
-        AppSharedModule,
-        HomeModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    // Add .withServerTransition() to support Universal rendering.
+    // The application ID can be any identifier which is unique on
+    // the page.
+    BrowserModule.withServerTransition({appId: 'mat-progress-buttons-demo-id'}),
+    TransferHttpCacheModule,
+    FormsModule,
+    AppRoutingModule,
+    AppSharedModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
