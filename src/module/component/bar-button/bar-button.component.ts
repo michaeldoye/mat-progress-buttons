6cefa98e48c6dfb91450665ddf10780cfb6e7304
Lukas Jakob <lu.jakob@googlemail.com>
2020-03-11 14:31:33 +0100

    feat(lib): add [active] and [disabled] inputs

--- src/module/component/bar-button/bar-button.component.ts
+++ src/module/component/bar-button/bar-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, HostListener } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener, OnChanges, SimpleChanges } from '@angular/core';
 import { MatProgressButtonOptions } from '../../mat-progress-buttons.interface';
 
 @Component({
@@ -7,13 +7,24 @@ import { MatProgressButtonOptions } from '../../mat-progress-buttons.interface';
   templateUrl: './bar-button.component.html',
   styleUrls: ['./bar-button.component.scss']
 })
-export class MatBarButtonComponent {
+export class MatBarButtonComponent implements OnChanges {
   @Input() options: MatProgressButtonOptions;
+  @Input() active: boolean;
+  @Input() disabled: boolean;
   @Output() btnClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
   @HostListener('click', ['$event'])
   public onClick(event: MouseEvent) {
     if (!this.options.disabled && !this.options.active) {
       this.btnClick.emit(event);
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.active) {
+      this.options.active = changes.active.currentValue;
+    }
+    if (changes.disabled) {
+      this.options.disabled = changes.disabled.currentValue;
     }
   }
 }
--- src/module/component/spinner-button/spinner-button.component.ts
+++ src/module/component/spinner-button/spinner-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, HostListener, EventEmitter } from '@angular/core';
+import { Component, Input, Output, HostListener, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { MatProgressButtonOptions } from '../../mat-progress-buttons.interface';
 
 @Component({
@@ -7,13 +7,25 @@ import { MatProgressButtonOptions } from '../../mat-progress-buttons.interface';
   templateUrl: './spinner-button.component.html',
   styleUrls: ['./spinner-button.component.scss']
 })
-export class MatSpinnerButtonComponent {
+export class MatSpinnerButtonComponent implements OnChanges {
   @Input() options: MatProgressButtonOptions;
+  @Input() active: boolean;
+  @Input() disabled: boolean;
+
   @Output() btnClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
   @HostListener('click', ['$event'])
   public onClick(event: MouseEvent) {
     if (!this.options.disabled && !this.options.active) {
       this.btnClick.emit(event);
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.active) {
+      this.options.active = changes.active.currentValue;
+    }
+    if (changes.disabled) {
+      this.options.disabled = changes.disabled.currentValue;
     }
   }
 }
