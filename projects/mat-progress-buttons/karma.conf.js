2fe60ab20e3689a0e93c65ddc29611ec0b9fba18
Andrei Bespamiatnov <adminsgz@gmail.com>
2020-12-20 00:44:38 +0100

    Possible fix for test runs

--- projects/mat-progress-buttons/karma.conf.js
+++ projects/mat-progress-buttons/karma.conf.js
@@ -3,39 +3,32 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
-    plugins: [
-      require("karma-jasmine"),
-      require("karma-chrome-launcher"),
-      require("karma-jasmine-html-reporter"),
-      require("karma-coverage-istanbul-reporter"),
-      require("@angular-devkit/build-angular/plugins/karma"),
-    ],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [require('karma-jasmine'), require('karma-chrome-launcher'), require('karma-jasmine-html-reporter'), require('karma-coverage'), require('@angular-devkit/build-angular/plugins/karma')],
     customLaunchers: {
       // Chrome setup for CI (Travis, Docker, ...)
       ChromeHeadlessCI: {
-        base: "ChromeHeadless",
-        flags: ["--no-sandbox"],
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'],
       },
     },
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require("path").join(
-        __dirname,
-        "../../coverage/mat-progress-buttons"
-      ),
-      reports: ["html", "lcovonly", "text-summary"],
-      fixWebpackSourcePaths: true,
+    coverageReporter: {
+      reporters: [
+        { type: 'html', subdir: 'report-html', dir: '../../coverage/' },
+        { type: 'lcov', subdir: 'report-lcov', dir: '../../coverage/' },
+        { type: 'text-summary' },
+      ],
     },
-    reporters: ["progress", "kjhtml"],
+    reporters: ['progress', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ['Chrome', 'ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true,
   });
